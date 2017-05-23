ec5d8e6fb5b5b11fe157fa0382183406e885b128
Aubrey Masten and Sara Schultz <aubrey@>
2017-05-23 14:36:07 -0700

    add fail message

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -4,9 +4,12 @@ $(document).ready(function() {
   $('#display').click(function() {
     var city = $('#city').val();
     $('#city').val('');
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('#humidity h3').text(`The humidity in ${city} is ${response.main.humidity}%`);
-
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+      .then(function(response) {
+        $('#humidity h3').text(`The humidity in ${city} is ${response.main.humidity}%`);
+    })
+    .fail(function(error) {
+      $('#humidity h3').text(error.responseJSON.message);
     });
   });
 });
